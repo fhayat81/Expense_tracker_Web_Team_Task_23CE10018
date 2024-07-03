b0526133f5c73b2d452afc37ecf7b8b296ed8f61
fhayat81 <fhayatkhan81@gmail.com>
2024-07-03 19:04:47 +0530

    Update Transactions.jsx

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MdDelete } from "react-icons/md";
 
 const Transactions = ({
@@ -7,7 +7,7 @@ const Transactions = ({
   expense,
   newTransaction,
   record,
-  deleteTransaction
+  deleteTransaction,
 }) => {
   const month = [
     "Jan",
@@ -26,12 +26,19 @@ const Transactions = ({
   const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   const [type, setType] = useState("Income");
-  const [title, setTitle] = useState("Untitled")
+  const [title, setTitle] = useState("Untitled");
   const [amount, setAmount] = useState(0);
   const [note, setNote] = useState("");
   const [incomeCategory, setIncomeCategory] = useState(income[0]);
   const [expenseCategory, setExpenseCategory] = useState(expense[0]);
   const [category, setCategory] = useState(incomeCategory);
+  const [date, setDate] = useState(null);
+  const [dte, setDte] = useState(new Date());
+
+  useEffect(() => {
+    const time = new Date(date);
+    setDte(time);
+  }, [date]);
 
   return (
     <div className="my-3 mx-4">
@@ -91,6 +98,19 @@ const Transactions = ({
                 >
                   Expense
                 </button>
+              </div>
+            </div>
+            {/* date*/}
+            <div className="my-2">
+              <div className="flex items-center gap-1">
+                <h1>Date:</h1>
+                <input
+                  type="date"
+                  onChange={(e) => {
+                    setDate(e.target.value);
+                  }}
+                  className="bg-slate-200 border-2 border-slate-600 rounded-lg px-2 py-1 w-[140px] text-right"
+                />
               </div>
             </div>
             {/* Category */}
@@ -180,8 +200,8 @@ const Transactions = ({
             <div className="flex justify-center items-center">
               <button
                 onClick={() => {
-                  newTransaction(title, amount, category, note, type);
-                  setTitle("Untitled")
+                  newTransaction(title, amount, category, note, type, dte);
+                  setTitle("Untitled");
                   setAmount(0);
                   setCategory(incomeCategory);
                   setNote("");
@@ -196,50 +216,77 @@ const Transactions = ({
         </div>
         {/* Transactions */}
         <div className="my-1">
-          {record.length !== 0 && 
-          record.slice().reverse().map((data)=>(
-            <div key={data.id} className="bg-white rounded-lg drop-shadow-lg w-[300px] sm:w-[600px]">
-            <div className="bg-slate-300 rounded-t-lg">
-              <h1 className="mx-2 my-1 text-lg font-semibold flex justify-between items-center">
-                <div className="flex gap-2">
-                  <p>{data.time.hours}:{data.time.minutes},</p>
-                  <p>{data.time.date}</p>
-                  <p>{month[data.time.month]}</p>
-                  <p>{data.time.year},</p>
-                  <p>{day[data.time.day]}</p>
+          {record.length !== 0 &&
+            record
+              .slice()
+              .reverse()
+              .map((data) => (
+                <div
+                  key={data.id}
+                  className="bg-white rounded-lg drop-shadow-lg w-[300px] sm:w-[600px]"
+                >
+                  <div className="bg-slate-300 rounded-t-lg">
+                    <h1 className="mx-2 my-1 text-lg font-semibold flex justify-between items-center">
+                      <div className="flex gap-2">
+                        <p>{data.time.date}</p>
+                        <p>{month[data.time.month]}</p>
+                        <p>{data.time.year},</p>
+                        <p>{day[data.time.day]}</p>
+                      </div>
+                      <div
+                        onClick={() => {
+                          deleteTransaction(data);
+                        }}
+                        className="bg-red-500 text-white rounded-lg m-1 active:scale-95"
+                      >
+                        <MdDelete className="text-2xl sm:text-3xl m-1" />
+                      </div>
+                    </h1>
+                  </div>
+                  <div className="mx-2 my-1">
+                    <div className="flex justify-between items-center">
+                      <h1 className="text-2xl">{data.title}</h1>
+                      {data.type === "Income" && (
+                        <h1 className="text-3xl mx-1 text-green-500 text-right">
+                          {" "}
+                          +{data.amount}
+                        </h1>
+                      )}
+                      {data.type === "Expense" && (
+                        <h1 className="text-3xl mx-1 text-red-500 text-right">
+                          {" "}
+                          -{data.amount}
+                        </h1>
+                      )}
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-center">
+                        <img
+                          src={data.cat.img}
+                          alt=""
+                          className="w-7 m-2 rounded-full"
+                        />
+                        <p>{data.cat.label}</p>
+                      </div>
+                      <div className="mx-2 group flex justify-end">
+                        <p className="bg-slate-300 rounded-lg p-1 hover:scale-95 cursor-pointer">
+                          Note
+                        </p>
+                        <p className="hidden group-hover:block absolute bottom-0 w-[280px] sm:w-[580px] bg-slate-200 p-2 rounded-lg drop-shadow-lg z-20 my-2">
+                          {data.note}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div onClick={()=>{deleteTransaction(data)}} className="bg-red-500 text-white rounded-lg m-1 active:scale-95">
-                  <MdDelete className="text-2xl sm:text-3xl m-1" />
-                </div>
-              </h1>
+              ))}
+          {record.length === 0 && (
+            <div className="bg-white drop-shadow-lg p-2 flex justify-center rounded-lg w-[300px] sm:w-[600px]">
+              <p className="text-center text-xl sm:text-2xl text-slate-500 font-semibold">
+                No Transactions
+              </p>
             </div>
-            <div className="mx-2 my-1">
-              <div className="flex justify-between items-center">
-                <h1 className="text-2xl">{data.title}</h1>
-                {data.type === "Income" && <h1 className="text-3xl mx-1 text-green-500 text-right"> +{data.amount}</h1>}
-                {data.type === "Expense" && <h1 className="text-3xl mx-1 text-red-500 text-right"> -{data.amount}</h1>}
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <img
-                    src={data.cat.img}
-                    alt=""
-                    className="w-7 m-2 rounded-full"
-                  />
-                  <p>{data.cat.label}</p>
-                </div>
-                <div className="mx-2 group flex justify-end">
-                  <p className="bg-slate-300 rounded-lg p-1 hover:scale-95 cursor-pointer">
-                    Note
-                  </p>
-                  <p className="hidden group-hover:block absolute bottom-0 w-[280px] sm:w-[580px] bg-slate-200 p-2 rounded-lg drop-shadow-lg z-20 my-2">
-                    {data.note}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          ))}
+          )}
         </div>
       </div>
     </div>
